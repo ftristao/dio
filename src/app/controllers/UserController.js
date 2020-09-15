b7576a640aa72a5b0db84aa9c6f8d1524bd9f924
ftristao <ftristao@bol.com.br>
2020-09-15 15:21:39 -0300

    Adicionada tela para solicitação de senha

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,7 +10,8 @@ export default {
           password: passwordGenerator(15, false)
       };
 
-      await Queue.add('RegistrationMail', { user});
+      await Queue.add('RegistrationMail', { user });
+    
       return res.json(user);
     }
 }
--- src/server.js
+++ src/server.js
@@ -4,10 +4,14 @@ import BullBoard from 'bull-board';
 import UserController from './app/controllers/UserController';
 import Queue from './app/lib/Queue';
 
+var path=require('path');
+
+
 const app = express();
 BullBoard.setQueues(Queue.queues.map(queue => queue.bull));
 
 app.use(express.json());
+app.use(express.static('public'));
 
 app.post('/users', UserController.store);
 app.use('/admin/queues', BullBoard.UI);
